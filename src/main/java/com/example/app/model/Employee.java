3c47673d9a9e04e7a71689a491eeed5c8a24e402
Alessander <alessander.logistica@gmail.com>
2020-06-02 20:10:27 -0300

    create the employee entity class

---
+++ src/main/java/com/example/app/model/Employee.java
@@ -0,0 +1,61 @@
+package com.example.app.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Employee {
+	private Integer id;
+	
+	@Column(name = "name")
+	private String name;
+
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+	@JoinColumn(foreignKey = @ForeignKey(name = "dept_id"), name = "dept_id")
+	private Departament departament;
+	
+	public Employee() {
+	}
+	
+	public Employee(String name, Departament departament) {
+		this.name = name;
+		this.departament = departament;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Departament getDepartament() {
+		return departament;
+	}
+
+	public void setDepartament(Departament departament) {
+		this.departament = departament;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + "]";
+	}
+	
+}
