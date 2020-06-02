83dec9842c0d28952a040f51183b54ce6b8c8649
Alessander <alessander.logistica@gmail.com>
2020-06-02 20:24:22 -0300

    add @Id annotation

--- src/main/java/com/example/app/model/Employee.java
+++ src/main/java/com/example/app/model/Employee.java
@@ -5,13 +5,20 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
 @Table
 public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "emp_seq")
+	@SequenceGenerator(initialValue = 1, name = "emp_seq", sequenceName = "employee_sequence")
 	private Integer id;
 	
 	@Column(name = "name")
